af3120157f911d856e0b59ba7074fd433d56529f
joaonunovalente <jnvalente@ua.pt>
2025-11-13 16:44:14 +0000

    Remove all unused libraries and plugins

--- assets/js/sticky-custom.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-
-/* ===== Stickyfill ===== */
-/* Ref: https://github.com/wilddeer/stickyfill */
-// Add browser support to position: sticky
-
-var elements = document.querySelectorAll('.sticky');
-Stickyfill.add(elements);
