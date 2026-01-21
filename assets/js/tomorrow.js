0f56519f49c8fd4f2c63a71a6a5f4a367349cc10
joaonunovalente <jnvalente@ua.pt>
2026-01-21 23:49:16 +0000

    Insert fallback menu HTML on error loading ementas

--- assets/js/tomorrow.js
+++ assets/js/tomorrow.js
@@ -161,7 +161,8 @@ document.addEventListener("DOMContentLoaded", () => {
       console.error("Erro ao carregar as ementas:", err);
       document.querySelectorAll(".item-desc").forEach(el => {
         stopLoadingAnimation(el);
-         el.textContent = "Erro ao carregar as ementas.";
+        el.innerHTML = fallbackMenuTomorrowHTML();  // insere o fallback no elemento
       });
     });
+
 });
